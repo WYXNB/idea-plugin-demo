6ed67a1ed50847fd57561ba75bc358d4797c6fe4
GitHub Action <action@github.com>
2022-06-16 03:39:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/wyxnb/ideaplugindemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wyxnb/ideaplugindemo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wyxnb.ideaplugindemo.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wyxnb.ideaplugindemo.MyBundle
 
 class MyProjectService(project: Project) {
 
